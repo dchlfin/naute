8f0beedb9e5d688718fddd0fb1af503649c16ba1
dchlfin <167969296+dchlfin@users.noreply.github.com>
2026-07-01 17:03:38 +0400

    feat: initialized add naute to recycler view functionality

--- app/src/main/java/com/example/naute/NauteAdapter.kt
+++ app/src/main/java/com/example/naute/NauteAdapter.kt
@@ -1,4 +1,34 @@
 package com.example.naute
 
-class NauteAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class NauteAdapter(private val context: Context, private val nautes: ArrayList<String>): RecyclerView.Adapter<NauteAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, position: Int): ViewHolder {
+        val nauteCard = LayoutInflater.from(context).inflate(R.layout.naute_card, viewGroup, false)
+        return ViewHolder(nauteCard)
+    }
+
+    override fun getItemCount(): Int {
+        return nautes.count()
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.bind(nautes[position])
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        fun bind(naute: String){
+            itemView.findViewById<TextView>(R.id.nauteTitle).text = naute
+            itemView.setOnClickListener {
+                Toast.makeText(context, naute, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 }
